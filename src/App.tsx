80edc3836e5cb2774aeaab73a4bfb5bc7f5d0003
LeslT <lesliejailine@hotmail.com>
2024-04-16 16:30:44 -0500

    final app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import styles from './App.module.css'
+import Alert from './components/Alert/Alert'
+import Form from './components/Form/Form'
+import Spinner from './components/Spinner/Spinner'
+import WeatherDetail from './components/WeatherDetail/WeatherDetail'
+import useWeather from './hooks/useWeather'
 function App() {
+  const { weather, loading, fetchWeather, hasWeatherData, notFound } = useWeather()
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
       <div className={styles.container}>
-        <p>1</p>
-        <p>2</p>
+        <Form fetchWeather={fetchWeather} />
+        {loading &&  <Spinner/> }
+        {hasWeatherData && <WeatherDetail weather={weather}/>}
+        {notFound && <Alert>Ciudad no encontrada</Alert> }
       </div>
     </>
   )
